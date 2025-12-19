c8dd34f72c0b9b1a8ec72ef578d4b8989cd3be89
girayeskici <ageskici@gmail.com>
2025-12-19 19:24:13 +0300

    Update anasayfaveli.dart

--- flutter_test_1/lib/veli/anasayfaveli.dart
+++ flutter_test_1/lib/veli/anasayfaveli.dart
@@ -6,18 +6,30 @@ import 'ilerleyisim_veli.dart';
 import 'ayarlar_veli.dart';
 
 class AnasayfaPageVeli extends StatefulWidget {
+<<<<<<< Updated upstream
+=======
+  const AnasayfaPageVeli({super.key, this.ad, this.email, this.tip});
+
+>>>>>>> Stashed changes
   final String? ad;
   final String? email;
   final String? tip;
 
+<<<<<<< Updated upstream
   const AnasayfaPageVeli({super.key, this.ad, this.email, this.tip});
 
+=======
+>>>>>>> Stashed changes
   @override
   State<AnasayfaPageVeli> createState() => _AnasayfaPageVeliState();
 }
 
 class _AnasayfaPageVeliState extends State<AnasayfaPageVeli> {
+<<<<<<< Updated upstream
   int _selectedIndex = 2; // Start with Bilgi Sayfasi (middle)
+=======
+  int _selectedIndex = 2; // Başlangıçta Bilgi Sayfası (ortadaki) açık olsun
+>>>>>>> Stashed changes
 
   late List<Widget> _pages;
 
@@ -41,6 +53,7 @@ class _AnasayfaPageVeliState extends State<AnasayfaPageVeli> {
 
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     // Blue theme color matching the requested design
     const primaryBlue = Color.fromARGB(255, 55, 107, 221);
 
@@ -63,6 +76,14 @@ class _AnasayfaPageVeliState extends State<AnasayfaPageVeli> {
             icon: Icon(Icons.sports_esports),
             label: 'Oyunlarım',
           ),
+=======
+    return Scaffold(
+      body: IndexedStack(index: _selectedIndex, children: _pages),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Ödevlerim'),
+          BottomNavigationBarItem(icon: Icon(Icons.games), label: 'Oyunlarım'),
+>>>>>>> Stashed changes
           BottomNavigationBarItem(
             icon: Icon(Icons.info),
             label: 'Bilgi Sayfası',
@@ -73,6 +94,16 @@ class _AnasayfaPageVeliState extends State<AnasayfaPageVeli> {
           ),
           BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Ayarlar'),
         ],
+<<<<<<< Updated upstream
+=======
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF2196F3), // Blue Theme
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType
+            .fixed, // 5 tane olduğu için fixed olması lazım
+        backgroundColor: Colors.white,
+>>>>>>> Stashed changes
       ),
     );
   }
